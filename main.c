b2c8ab962731e193202284e48b3cffe6d864602f
jindandelion <gieoun524@sookmyung.ac.kr>
2019-10-01 16:14:06 +0900

    05(All)

--- main.c
+++ main.c
@@ -2,9 +2,32 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/*Practice3*/
+
 int main(int argc, char *argv[]) {
 	
+	/*Practice1*/
+	
+	int x;
+	
+	printf("input a number:");
+	scanf("%d",&x);
+	
+	if(x>0)
+		printf("positive\n", x);
+	else
+		printf("non-positive\n",-x);
+	/*Practice2*/
+	
+	int a;
+	printf("input a number:");
+	scanf("%d", &a);
+	
+	if (a>0)
+		printf("|x| = %d\n", a);
+	else
+		printf("|x| = %d\n", -a);
+		
+	/*Practice3*/
 	int input;
 	
 	printf("input a number");
