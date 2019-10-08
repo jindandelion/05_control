b6750067ab34c72dc75d210f5b8d1af02cca7f4b
jindandelion <gieoun524@sookmyung.ac.kr>
2019-10-08 15:12:50 +0900

    191008

--- main.c
+++ main.c
@@ -5,46 +5,13 @@
 
 int main(int argc, char *argv[]) {
 	
-	/*Practice1*/
+	int i=0, meter;
 	
-	int x;
-	
-	printf("input a number:");
-	scanf("%d",&x);
-	
-	if(x>0)
-		printf("positive\n", x);
-	else
-		printf("non-positive\n",-x);
-	/*Practice2*/
-	
-	int a;
-	printf("input a number:");
-	scanf("%d", &a);
-	
-	if (a>0)
-		printf("|x| = %d\n", a);
-	else
-		printf("|x| = %d\n", -a);
-		
-	/*Practice3*/
-	int input;
-	
-	printf("input a number");
-	scanf("%d", &input);
-	
-	if(input>0)
+	while (i<3)
 	{
-		printf("positive\n");
-		
-	}
-	else if(input<0)
-	{
-		printf("negative\n");
-	}
-	else
-	{
-		printf("0\n"); 
+			meter=i*1609;
+			printf("%d 마일은 %d 미터입니다.\n",i, meter);
+			i++;
 	}
 	return 0;
 }
