0021e981da0207c971b8d7767c047365ef225b85
jindandelion <gieoun524@sookmyung.ac.kr>
2019-10-08 16:23:25 +0900

    Practice7

--- main.c
+++ main.c
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/* Practice 5*/
+/* Practice 6*/
 int main(int argc, char *argv[]) {
 	
-	int num = 0;
-	char c;
+	int answer = 59;
+	int input;
+	int cnt=0;//처음에 0으로 초기화 하고 얼만에 맞췄냐 세는 변수를 설정. count의 줄임말. 
 	
-	printf("input a string :");
-	
-	while((c = getchar()) != '\n')
+	do
 	{
-			if(c <= '9' && c >= '0')
-			{
-				num++;
-			}
-	}
-	
-	printf("%i\n", num);
+		printf("guess a number:");
+		scanf("%d",&input);
+		if (input < answer)
+		{
+			printf("low!\n");
+		}
+		else if (input > answer)
+		{
+			printf("high!\n");
+		}
+		cnt++;
+	 } 
+	 while(input != answer);
+	 
+	 printf("Congratulation. trial : %i\n", cnt);
+	 
 	return 0;
 }
